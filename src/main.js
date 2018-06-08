a45cd31c5b67bfa390f9c8f2eb69d0a92636b48d
Carlos Torres García <togacharls@gmail.com>
2018-06-08 13:13:28 +0200

    Added axios dependency and referenced by Vue.$http

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import axios from 'axios'
 import App from './App'
 
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
